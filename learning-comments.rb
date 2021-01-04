eaa57f0bcfb9157dceafa5998f2737a49d8b9ec4
Gabriel Castro <gabriel.castro1028@gmail.com>
2021-01-04 20:35:57 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,6 @@
+[10,20,30,40].reduce(0){ |total,num| total + num }
+10,20,30,40.recude(100){ |total, num| total + num }
+
+
+[10,20,30,40].select { |num| num > 25 }
+[10,20,30,40].reject { num| num > 25 }
